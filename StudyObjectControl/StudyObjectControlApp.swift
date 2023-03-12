0e84880765c0dd0286dd35aa162d710f983e5793
YimKeul <leesjun29@gmail.com>
2023-03-12 23:39:44 +0900

    Initial Commit

---
+++ StudyObjectControl/StudyObjectControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyObjectControlApp.swift
+//  StudyObjectControl
+//
+//  Created by yimkeul on 2023/03/12.
+//
+
+import SwiftUI
+
+@main
+struct StudyObjectControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
